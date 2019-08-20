2c70d74e05fc24cac15096333587b3eea0a6577e
Erin Gurley <erinmgurley@gmail.com>
2019-08-19 21:22:39 -0700

    added color selector, shape component, and moved text rendering into textSelector component

---
+++ src/components/Shape.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Shape({ text, color, backgroundColor }) {
+  return (
+    <>
+      <div style={{ 
+        width: '100px', 
+        height: '100px',
+      }}>
+        <pre style ={{
+          color: color,
+          backgroundColor: backgroundColor,
+        }}>{text}</pre>
+      </div>
+    </>
+  );
+}
+
+Shape.propTypes = {
+  text: PropTypes.string.isRequired,
+  color: PropTypes.string.isRequired,
+  backgroundColor: PropTypes.string.isRequired,
+};
+
+export default Shape;
---
+++ src/components/TextSelector.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function TextSelector({ text, handleTextChange, handleSubmit }) {
+  return (
+    <>
+        <form onSubmit={handleSubmit}>
+          <input 
+            placeholder="Full Name" 
+            name="text" value={text} 
+            onChange={handleTextChange}></input>
+        </form>
+        {text}
+    </>
+  );
+}
+
+TextSelector.propTypes = {
+  text: PropTypes.string.isRequired,
+  handleTextChange: PropTypes.func.isRequired,
+  handleSubmit: PropTypes.func.isRequired
+};
+
+export default TextSelector;
